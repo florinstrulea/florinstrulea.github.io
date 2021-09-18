dbe35a27361cd27672d50561e562919fabf45b4a
Florin Strulea <coolfloboy@gmail.com>
2021-09-18 15:53:19 +0200

    j'ai finalisé la section projets. j'ai ajouté les buttons discover more, les animations, les box shadows et les border radius

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const body = document.querySelector("body");
 const label = document.querySelector(".label");
 const button = document.querySelector(".switch");
 const cv = document.querySelector(".cv");
+const projects = document.querySelectorAll(".project");
 
 checkbox.addEventListener("click", () => {
 	body.classList.toggle("light");
@@ -10,4 +11,7 @@ checkbox.addEventListener("click", () => {
 	label.classList.toggle("black");
 	button.classList.toggle("light");
 	cv.classList.toggle("text-white");
+	for (let project of projects) {
+		project.classList.toggle("box-shadow-dark");
+	}
 });
