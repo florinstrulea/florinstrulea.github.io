23a976767c026a6f669637db9ba137e94be50e1a
Florin Strulea <coolfloboy@gmail.com>
2021-09-18 17:55:02 +0200

    ajouté la fonctionalité de changement des langues anglais et français

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ const label = document.querySelector(".label");
 const button = document.querySelector(".switch");
 const cv = document.querySelector(".cv");
 const projects = document.querySelectorAll(".project");
+const french = document.querySelector("#french");
+const english = document.querySelector("#english");
+const selectors = document.querySelectorAll(
+	"#greeting, #job, #about-me-title, #my-projects, #about-me-text, #toolbox, #loading"
+);
 
 checkbox.addEventListener("click", () => {
 	body.classList.toggle("light");
@@ -13,5 +18,16 @@ checkbox.addEventListener("click", () => {
 	cv.classList.toggle("text-white");
 	for (let project of projects) {
 		project.classList.toggle("box-shadow-dark");
+	}
+});
+
+french.addEventListener("click", () => {
+	for (let select of selectors) {
+		select.textContent = select.dataset.fr;
+	}
+});
+english.addEventListener("click", () => {
+	for (let select of selectors) {
+		select.textContent = select.dataset.en;
 	}
 });
