2ce2d02714d13226635ac8c86fb86b8cafb9e47b
Florin Strulea <coolfloboy@gmail.com>
2021-09-28 10:36:30 +0200

    modif au niveau du button dark/light

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const selectors = document.querySelectorAll(
 	"#greeting, #job, #about-me-title, #my-projects, #about-me-text, #toolbox, #loading , #freetime, #home, .all-projects, #title, .project-text, #page-title, #description, .btn-project, .btn-project-go"
 );
 
-if (window.location.href.indexOf("index.html") > -1) {
+if (window.location.href.indexOf("index" || "Portfolio/ ") > -1) {
 	document.querySelector("#checkbox").addEventListener("click", () => {
 		body.classList.toggle("light");
 		label.classList.toggle("white");
